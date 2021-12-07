c8dd8f0bda674cce3425cced294e1deb039ac567
Brett Martin <bamartindev@gmail.com>
2021-12-07 01:21:07 -0600

    small change to just use the starting values as that appears to work

--- src/Solutions/DaySeven.hs
+++ src/Solutions/DaySeven.hs
@@ -4,16 +4,16 @@ module Solutions.DaySeven
   )
 where
 
-import Data.List (sort)
+import Data.List (nub, sort)
 
 -- Pretty simple brute force.  There is probably a mathy way to do this to find the most central point to all points
 -- and just do the distance compute once instead of against the whole range :)
 
 d7p1 :: [Int] -> IO ()
-d7p1 input = print $ minimum $ map (totalDistance input) [minimum input .. maximum input]
+d7p1 input = print $ minimum $ map (totalDistance input) $ nub input
 
 d7p2 :: [Int] -> IO ()
-d7p2 input = print $ minimum $ map (totalDistance' input) [minimum input .. maximum input]
+d7p2 input = print $ minimum $ map (totalDistance' input) $ nub input
 
 totalDistance :: [Int] -> Int -> Int
 totalDistance p i = sum $ map (\x -> curry distance x i) p
