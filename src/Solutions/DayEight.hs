ecc074795f9a396ecc88301cb7547e38cb06116f
Brett Martin <bamartindev@gmail.com>
2021-12-08 17:14:48 -0600

    some cleanup, removed map and just using list

--- src/Solutions/DayEight.hs
+++ src/Solutions/DayEight.hs
@@ -4,7 +4,7 @@ module Solutions.DayEight
   )
 where
 
-import Data.List (intersect, sort, subsequences)
+import Data.List (findIndex, intersect, sort, subsequences, (\\))
 import Data.List.Split (splitOn)
 import qualified Data.Map.Strict as M
 
@@ -16,34 +16,11 @@ d8p1 input = do
 d8p2 :: [String] -> IO ()
 d8p2 input = do
   let entries = map parseEntry input
-  let mappings = map mkSimpleMapping entries
+  print $ sum $ map calc entries
 
-  let entries' = zip entries mappings
-  let mappings = map (invertMap . completeEntryMap [9, 6, 0, 5, 2, 3]) entries'
-
-  print $ sum $ zipWith (curry decode) entries mappings
-
-type InvertedMapping = M.Map Pattern Int
-
-invertMap :: Mapping -> InvertedMapping
-invertMap m = M.fromList im
-  where
-    im = map (\(a, b) -> (sort b, a)) $ M.toList m
-
-completeEntryMap :: [Int] -> (Entry, Mapping) -> Mapping
-completeEntryMap [] (_, m) = m
-completeEntryMap n (e, m) = completeEntryMap (tail n) (e, m')
-  where
-    m' = M.fromList $ findMap e m (head n) : M.toList m
-
-decode :: (Entry, InvertedMapping) -> Int
-decode (Entry _ o, m) = foldl addDigit 0 $ map decode' o
-  where
-    decode' e = case M.lookup (sort e) m of
-      Just v -> v
-      Nothing -> error $ "Failed to decode! " ++ sort e
-    addDigit n d = 10 * n + d
-
+--------------
+-- Start P1
+--------------
 type Pattern = String
 
 data Entry = Entry
@@ -62,26 +39,40 @@ parseEntry e = Entry patterns' output'
 countEasy :: Entry -> Int
 countEasy (Entry _ output) = length $ filter (\x -> length x `elem` [2, 3, 4, 7]) output
 
-type Mapping = M.Map Int Pattern
-
-mkSimpleMapping :: Entry -> Mapping
-mkSimpleMapping (Entry s o) = M.fromList $ filter (\x -> fst x /= -1) $ map m (s ++ o)
+--------------
+-- Start P2
+--------------
+calc :: Entry -> Int
+calc (Entry p o) = foldl addDigit 0 $ map (decode' mapped) o
   where
-    m p = case length p of
-      2 -> (1, p)
-      3 -> (7, p)
-      4 -> (4, p)
-      7 -> (8, p)
-      _ -> (-1, p)
+    addDigit n d = 10 * n + d
+    mapped = map (getPattern p) [0 .. 9]
 
-findMap :: Entry -> Mapping -> Int -> (Int, Pattern)
-findMap (Entry s _) m 9 = (9, head [e | e <- s, length e == 6, e `fullyContains` M.findWithDefault "" 4 m])
-findMap (Entry s _) m 6 = (6, head [e | e <- s, length e == 6, not $ e `fullyContains` M.findWithDefault "" 7 m])
-findMap (Entry s _) m 0 = (0, head [e | e <- s, length e == 6, e `notElem` M.elems m])
-findMap (Entry s _) m 5 = (5, head [e | e <- s, length e == 5, M.findWithDefault "" 6 m `fullyContains` e])
-findMap (Entry s _) m 2 = (2, head [e | e <- s, length e == 5, M.findWithDefault "" 8 m `fullyContains` e && not (M.findWithDefault "" 9 m `fullyContains` e)])
-findMap (Entry s _) m 3 = (3, head [e | e <- s, length e == 5, e `notElem` M.elems m])
-findMap _ _ _ = error "Unimplemented map finder"
+decode' :: [Pattern] -> Pattern -> Int
+decode' ps p = case findIndex f ps of
+  Just i -> i
+  Nothing -> error "Couldn't find the pattern in the list of patterns"
+  where
+    f a = sort p == sort a
 
-fullyContains :: String -> String -> Bool
-fullyContains a b = b `intersect` a == b && a /= b
+getPattern :: [Pattern] -> Int -> Pattern
+getPattern p 0 = head $ filter (\x -> length x == 6 && x `notElem` [getPattern p 6, getPattern p 9]) p
+getPattern p 1 = getSimple p 2
+getPattern p 2 = head $ filter (\x -> length x == 5 && x `notElem` [getPattern p 3, getPattern p 5]) p
+getPattern p 3 = head $ filter (matchPattern (getPattern p 1) 5) p
+getPattern p 4 = getSimple p 4
+getPattern p 5 = head $ filter (matchPattern (diff4 p) 5) p
+getPattern p 6 = head $ filter (\x -> x /= getPattern p 9 && matchPattern (diff4 p) 6 x) p
+getPattern p 7 = getSimple p 3
+getPattern p 8 = getSimple p 7
+getPattern p 9 = head $ filter (matchPattern (getPattern p 4) 6) p
+getPattern _ _ = error "Cannot find pattern"
+
+matchPattern :: Pattern -> Int -> (Pattern -> Bool)
+matchPattern val len x = length x == len && (val `intersect` x == val)
+
+getSimple :: [Pattern] -> Int -> Pattern
+getSimple p len = head $ filter (\x -> length x == len) p
+
+diff4 :: [Pattern] -> Pattern
+diff4 p = getPattern p 4 \\ getPattern p 1
